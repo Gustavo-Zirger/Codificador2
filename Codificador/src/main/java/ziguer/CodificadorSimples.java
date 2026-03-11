8d1f3a7bba6d104126651a49d885b4b30514b299
Gustavo-Zirger <gustavo.zirger@edu.pucrs.br>
2026-03-11 18:46:48 +0000

    MinhaVersao

--- Codificador/src/main/java/ziguer/CodificadorSimples.java
+++ Codificador/src/main/java/ziguer/CodificadorSimples.java
@@ -8,18 +8,20 @@ public class CodificadorSimples implements Codificador {
     }
 
     public LocalDate getDataCriacao() {
-        return LocalDate.of(2025, 03, 13);
+        return LocalDate.of(2026, 03, 9);
     }
 
     public int getNivelSeguranca(){
-        return 1;
+        return 2;
     }
 
     public String codifica(String str) {
         StringBuilder encoded = new StringBuilder();
+        int sum = 1;
 
         for (char c : str.toCharArray()) {
-            encoded.append((char) (c + 1));
+            encoded.append((char) (c + (str.length()-sum)));
+            sum++;
         }
 
         return encoded.toString();
@@ -27,9 +29,11 @@ public class CodificadorSimples implements Codificador {
 
     public String decodifica(String str) {
         StringBuilder encoded = new StringBuilder();
+        int sum = 1;
         
         for (char c : str.toCharArray()) {
-            encoded.append((char) (c - 1));
+            encoded.append((char) (c - (str.length()-sum)));
+            sum++;
         }
         
         return encoded.toString();
