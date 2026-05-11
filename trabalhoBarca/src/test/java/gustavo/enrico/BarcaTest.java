faef275a6bf5d68832ccda1a80fcea5a0eefc257
Gustavo-Zirger <gustavo.zirger@edu.pucrs.br>
2026-05-11 18:47:32 +0000

    casos testes funcionando

--- trabalhoBarca/src/test/java/gustavo/enrico/BarcaTest.java
+++ trabalhoBarca/src/test/java/gustavo/enrico/BarcaTest.java
@@ -33,19 +33,21 @@ assentos:
 
     peso   | fileira | assento | resultado
            |         |         |    
-     1     |   01    |   01    | aprovado(3)
-     1     |   21    |   19    | inválido(2)
+     1     |   01    |   01    | inválido(1)
+     1     |   00    |   01    | inválido(0)
      1     |   01    |   21    | inválido(0)
-     1     |   21    |   19    | inválido(2)
-     1     |   01    |   21    | inválido(0)     
-     2     |   41    |   01    | aprovado(3)   
-     2     |   01    |   19    | inválido(2)   
-     2     |   41    |   21    | inválido(0)  
-     3     |   31    |   01    | aprovado(3)   
-     3     |   01    |   19    | aprovado(3)     
-     3     |   41    |   19    | aprovado(3)   
-     3     |   41    |   21    | inválido(0)
-     3     |   41    |   21    | inválido(1)
+     1     |   04    |   01    | aprovado(3)
+     1     |   21    |   01    | inválido(2)
+
+     2     |   40    |   01    | inválido(1)
+     2     |   61    |   01    | inválido(0)
+     2     |   40    |   21    | inválido(0)
+     2     |   44    |   01    | aprovado(3)
+     2     |   05    |   01    | inválido(2)
+
+     3     |   33    |   01    | aprovado(3)
+     3     |   09    |   01    | aprovado(3)
+     3     |   45    |   01    | aprovado(3)
  */
 
 public class BarcaTest {
@@ -54,12 +56,63 @@ public class BarcaTest {
     @ParameterizedTest
     @CsvSource({ // "lugar, valor experado"
         "F01A01, 1",
-        "F00A01, 0"
+        "F00A01, 0",
+        "F01A21, 0",
+        "F04A01, 3",
+        "F21A01, 2"
     })
-    void testOcupaLugar(String lugar, int esperado) {
+    void primeiroPeso(String lugar, int esperado) {
         Barca barca = new Barca();
-        for(int f = 1; f <=3; f++){ // cont 60 fileira 1-3 cheia
+        for(int f = 1; f <= 3; f++){ // cont 60 fileira 1-3 cheia
+            for(int a = 1; a <= 20; a++){
+                barca.ocupaLugarSemVerificacao(f,a);
+            }
+        }
+        assertEquals(esperado, barca.ocupaLugar(lugar));
+    }
+
+    @Test
+    @DisplayName("casos segunda distribuição de peso")
+    @ParameterizedTest
+    @CsvSource({ // "lugar, valor experado"
+        "F40A01, 1",
+        "F61A01, 0",
+        "F40A21, 0",
+        "F44A01, 3",
+        "F05A01, 2"
+    })
+    void segundoPeso(String lugar, int esperado) {
+        Barca barca = new Barca();
+        for(int f = 1; f <= 5; f++){ // cont 100 fileira 1-5 cheia
+            for(int a = 1; a <= 20; a++){
+                barca.ocupaLugarSemVerificacao(f,a);
+            }
+        }
+        for(int f = 40; f <= 43; f++){ // cont 160 fileira 40-43 cheia
             for(int a = 1; a<=20; a++){
+                barca.ocupaLugarSemVerificacao(f,a);
+            }
+        }
+        assertEquals(esperado, barca.ocupaLugar(lugar));
+    }
+
+    @Test
+    @DisplayName("casos terceira distribuição de peso")
+    @ParameterizedTest
+    @CsvSource({ // "lugar, valor experado"
+        "F33A01, 3",
+        "F09A01, 3",
+        "F46A01, 3"
+    })
+    void terceiroPeso(String lugar, int esperado) {
+        Barca barca = new Barca();
+        for(int f = 1; f <= 5; f++){ // cont 100 fileira 1-5 cheia
+            for(int a = 1; a <= 20; a++){
+                barca.ocupaLugarSemVerificacao(f,a);
+            }
+        }
+        for(int f = 40; f <= 45; f++){ // cont 200 fileira 40-45 cheia
+            for(int a = 1; a <= 20; a++){
                 barca.ocupaLugarSemVerificacao(f,a);
             }
         }
