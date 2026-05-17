124d1f82289f8d47608f0ea22e96453135a81639
Gustavo-Zirger <gustavo.zirger@edu.pucrs.br>
2026-05-17 17:03:50 +0000

    retornei o nome original da nossa classe, e nomeei a da Milena

--- trabalhoBarca/src/main/java/gustavo/enrico/Barca.java
+++ trabalhoBarca/src/main/java/gustavo/enrico/Barca.java
@@ -1,109 +1,100 @@
 package gustavo.enrico;
-/**
- * Grupo 13: Milena Schultz
- */
-public class Barca {
 
-    private int qtdPassageiros;
-    private static final int TOTALFILEIRAS = 60;
-    private static final int TOTALASSENTOS = 20;  
-    private final boolean [][]assentos;
+public class Barca { 
 
-    public Barca() {
-        this.assentos = new boolean [TOTALFILEIRAS][TOTALASSENTOS];
-        this.qtdPassageiros = 0;
-    }
-    /**
-     * verifica se um assento especifico esta ocupado
-     * @param assento
-     * @param fila
-     * @return
-     */
-    public boolean isOcupado(int fila, int assento) {
-        //troca para o indice 0 do java para nao dar erro de logica
-        int f = fila - 1;
-        int a = assento - 1;
-        // alterei o <= era <
-        if (f >= 0 && f <= TOTALFILEIRAS && a >= 0 && a <= TOTALASSENTOS){
-            return assentos[f][a];
-        }
-        return true;
-    }
-    /**
-     * define as condicoes de distribuicao de peso
-     * @param assento
-     * @param fila
-     * @return
-     */
-    private boolean distribuicaoPeso(int fileira) {
-        if (qtdPassageiros >= 0 && qtdPassageiros <= 99) {
-            return fileira >= 1 && fileira <= 20;
-        }
-        if (qtdPassageiros >= 100 && qtdPassageiros <= 199) {
-            return fileira >= 40 && fileira <= 60;
-        }
-        return true;
-    }
-   /**
-    * metodo auxiliar projetado para facilitar testes (montagem de cenario)
-    * ocupa o lugar sem verificacao, isto eh, simplesmente ocupa o lugar 
-    * sem validar nenhuma das regras de ocupacao
-    * @param fila
-    * @param assento
-    */
-    public void ocupaLugarSemVerificacao(int fila, int assento) {
-        int f = fila - 1;
-        int a = assento - 1;
-        // alterei o <= era <
-        if (f >= 0 && f <= 60 && a >= 0 && a <= 20) { 
-            if(!assentos[f][a]) {
-                assentos[f][a] = true;
-                this.qtdPassageiros++;
-            }
-        }
-    }
-    /**
-     * retorna:
-     * 0 - identificador de assento invalido
-     * 1 - assento ocupado
-     * 2 - assento bloqueado devido a distribuicao de peso
-     * 3 - ok, assento atribuido ao passageiro
-     * @param assentoInformado
-     * @return
-     */
-    public int ocupaLugar(String assentoInformado) {
-        if (assentoInformado == null || assentoInformado.length() != 6) {
-            return 0;
-        }
-        try {
-            int f = Integer.parseInt(assentoInformado.substring(1, 3));
-            int a = Integer.parseInt(assentoInformado.substring(4, 6));
-            
-            if (f < 1 || f > 60 || a < 1 || a >20) {
-                return 0;
-            }
-            if (!distribuicaoPeso(f)) {
-                return 2;
-            }
-            if (isOcupado(f, a)) {
-                return 1;
-            }
-            
-            this.assentos[f - 1][a - 1] = true;
-            this.qtdPassageiros++;
-            return 3;
+    int[][] lugares;
+    int cont;
 
-        } catch (NumberFormatException e) {
-            return 0;
-        }
+    public Barca(){ 
+        this.lugares = new int[60][20];
+        cont = 0;
+    } 
+
+ 
+
+    // Método auxiliar projetado para facilitar testes (montagem de cenário) 
+    // Ocupa o lugar sem verificação, isto é, simplesmente ocupa o lugar 
+    // sem validar nenhuma das regras de ocupação 
+    public void ocupaLugarSemVerificacao(int fila, int assento){
+        lugares[fila-1][assento-1] = 1;
+        cont++; 
     }
-    /**
+    
+
+ 
+
+    /* 
+
+     * Retorna: 
+     * 0 – Identificador de assento inválido 
+     * 1 – Assento ocupado 
+     * 2 – Assento bloqueado devido a distribuição de peso 
+     * 3 – Ok, assento atribuído ao passageiro. 
      * 
-     * @param f
-     * @param a
-     * @return
-     */
-    public String formataAssento(int f, int a) {
-        return String.format("F%02dA%02d", f, a);
+     * casos de teste:
+        [0,100] - sentam na frente 1 a 20
+        [101,200] - sentam atrás 40 a 60
+        [201,1200] - sentam em qualquer lugar
+     */ 
+
+    public int ocupaLugar(String assentoInformado){ 
+        //F01A01 F[0] 0[1] 1[2] A[3] 0[4] 1[5]
+        // char[] partes = assentoInformado.toCharArray();
+        // int fileira = Character.getNumericValue(partes[1]) * 10 + Character.getNumericValue(partes[2]);
+        // int assento = Character.getNumericValue(partes[4]) * 10 + Character.getNumericValue(partes[5]);
+
+        int fileira = Integer.parseInt(assentoInformado.substring(1, 3));
+        int assento = Integer.parseInt(assentoInformado.substring(4));
+
+
+        if(assentoInformado.substring(0,1).equals("F") && assentoInformado.substring(3,4).equals("A") && fileira > 0 && fileira <= 60 && assento > 0 && assento <= 20){
+
+            if(cont <= 100){
+                if(fileira <= 20){
+                    if(lugares[fileira-1][assento-1] == 0){
+                        lugares[fileira-1][assento-1] = 1;
+                        cont++;
+                        return 3; // assento atribuído ao passageiro
+                    }
+                    else{
+                        return 1; // assento ocupado
+                    }
+                }
+                else{
+                    return 2; // bloqueado distribuição de peso
+                } 
+            }
+            else if(cont > 100 && cont <= 200){
+                if(fileira >= 40 && fileira <= 60){
+                    if(lugares[fileira-1][assento-1] == 0){
+                        lugares[fileira-1][assento-1] = 1;
+                        cont++;
+                        return 3; // assento atribuído ao passageiro
+                    }
+                    else{
+                        return 1; // assento ocupado
+                    }
+                }
+                else{
+                    return 2; // bloqueado distribuição de peso
+                } 
+            }
+            else{
+                if(cont < 1200){
+                    if(lugares[fileira-1][assento-1] == 0){
+                        lugares[fileira-1][assento-1] = 1;
+                        cont++;
+                        return 3; // assento atribuído ao passageiro
+                    }
+                    else{
+                        return 1; // assento ocupado
+                    }                
+                }
+                else{
+                    return 1; // assento ocupado
+                }
+            }
+        }
+        return 0; // assento inválido
     }
-}
+} 
---
+++ trabalhoBarca/src/main/java/gustavo/enrico/BarcaMilena.java
@@ -0,0 +1,107 @@
+package gustavo.enrico;
+/**
+ * Grupo 13: Milena Schultz
+ */
+public class BarcaMilena {
+
+    private int qtdPassageiros;
+    private static final int TOTALFILEIRAS = 60;
+    private static final int TOTALASSENTOS = 20;  
+    private final boolean [][]assentos;
+
+    public BarcaMilena() {
+        this.assentos = new boolean [TOTALFILEIRAS][TOTALASSENTOS];
+        this.qtdPassageiros = 0;
+    }
+    /**
+     * verifica se um assento especifico esta ocupado
+     * @param assento
+     * @param fila
+     * @return
+     */
+    public boolean isOcupado(int assento, int fila) {
+        //troca para o indice 0 do java para nao dar erro de logica
+        int f = fila - 1;
+        int a = assento - 1;
+        if (f >= 0 && f < TOTALFILEIRAS && a >= 0 && a < TOTALASSENTOS){
+            return assentos[f][a];
+        }
+        return true;
+    }
+    /**
+     * define as condicoes de distribuicao de peso
+     * @param assento
+     * @param fila
+     * @return
+     */
+    private boolean distribuicaoPeso(int fileira) {
+        if (qtdPassageiros >= 0 && qtdPassageiros <= 99) {
+            return fileira >= 1 && fileira <= 20;
+        }
+        if (qtdPassageiros >= 100 && qtdPassageiros <= 199) {
+            return fileira >= 40 && fileira <= 60;
+        }
+        return true;
+    }
+   /**
+    * metodo auxiliar projetado para facilitar testes (montagem de cenario)
+    * ocupa o lugar sem verificacao, isto eh, simplesmente ocupa o lugar 
+    * sem validar nenhuma das regras de ocupacao
+    * @param fila
+    * @param assento
+    */
+    public void ocupaLugarSemVerificacao(int fila, int assento) {
+        int f = fila - 1;
+        int a = assento - 1;
+        if (f >= 0 && f < 60 && a >= 0 && a < 20) { 
+            if(!assentos[f][a]) {
+                assentos[f][a] = true;
+                this.qtdPassageiros++;
+            }
+        }
+    }
+    /**
+     * retorna:
+     * 0 - identificador de assento invalido
+     * 1 - assento ocupado
+     * 2 - assento bloqueado devido a distribuicao de peso
+     * 3 - ok, assento atribuido ao passageiro
+     * @param assentoInformado
+     * @return
+     */
+    public int ocupaLugar(String assentoInformado) {
+        if (assentoInformado == null || assentoInformado.length() != 6) {
+            return 0;
+        }
+        try {
+            int f = Integer.parseInt(assentoInformado.substring(1, 3));
+            int a = Integer.parseInt(assentoInformado.substring(4, 6));
+            
+            if (f < 1 || f > 60 || a < 1 || a >20) {
+                return 0;
+            }
+            if (!distribuicaoPeso(f)) {
+                return 2;
+            }
+            if (isOcupado(f, a)) {
+                return 1;
+            }
+            
+            this.assentos[f - 1][a - 1] = true;
+            this.qtdPassageiros++;
+            return 3;
+
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    /**
+     * 
+     * @param f
+     * @param a
+     * @return
+     */
+    public String formataAssento(int f, int a) {
+        return String.format("F%02dA%02d", f, a);
+    }
+}
